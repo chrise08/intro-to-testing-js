60fa59434b41bad920be3ef94d242446e93b746f
Chris Eckert <chris.eckert08@gmail.com>
2020-04-23 15:07:32 -0500

    add the first test and solution for intro-to-testing

--- code.js
+++ code.js
@@ -2,3 +2,15 @@
 function helloWorld() {
     return "Hello, World!";
 }
+
+function isNumeric(input) {
+    if (isNaN(input)) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+function sayHello() {
+
+}
--- tests.js
+++ tests.js
@@ -13,3 +13,33 @@ describe('helloWorld', function() {
       expect(helloWorld()).not.toBe(undefined);
     });
 });
+
+describe("isNumeric", function () {
+    it('should be a defined function', function () {
+        expect(typeof isNumeric).toBe("function");
+    });
+    it('should return a boolean value when called', function () {
+        expect(typeof isNumeric()).toBe("boolean");
+    });
+    it('should return true when passed the number 5', function () {
+        expect(isNumeric(5)).toBe(true);
+    });
+    it('should should return false when passed the string "Jim Henson"', function () {
+        expect(isNumeric("Jim Henson")).toBe(false);
+    });
+    it('should return false when passed the array [1,2,3]', function () {
+        expect(isNumeric([1,2,3])).toBe(false);
+    });
+    it('should return false when passed an object', function () {
+        expect(isNumeric({name: "Carole Baskin", occupation: "Big Cat Enthusiast"})).toBe(false);
+    });
+    it('should return false when passed no arguments', function () {
+        expect(isNumeric()).toBe(false);
+    });
+})
+
+describe("sayHello", function () {
+    it('should be a defined function', function () {
+        expect(typeof sayHello).toBe("function");
+    });
+})
